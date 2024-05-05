ebfa5dab64558aa69aeede46316ab6ed92178904
DorzelRamos <dorzel.ramos@gmail.com>
2024-05-05 10:18:36 -0500

    switching to new files

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
 $(function () {
     gsap.registerPlugin(GSDevTools, DrawSVGPlugin);
     let mobileNav = $("#mobile-nav");
-    let mainContent = $("#main-content");
-    let openMobNav = $("#openMobNav");
-    let closeMobNav = $("#closeMobNav");
-    // console.log(mobileNav);
-    mobileNav.css("visibility", "hidden");
-    openMobNav.click(OpenningMobNav);
-    closeMobNav.click(ClosingMobNav);
-    function OpenningMobNav() {
-        mobileNav.css("visibility", "visible"); 
-        mainContent.css("visibility", "hidden");
-    }
-    function ClosingMobNav() {
-        mobileNav.css("visibility", "hidden"); 
-        mainContent.css("visibility", "visible");
-    }
+    let homeContent = $("#home-content");
+    let menu = $(".hamburger-menu");
+
+    menu.click(()=> {
+        // mobileNav.animate({display: "flex", zIndex: "1"}, 200);
+        mobileNav.toggleClass("show");
+    })
+
+    
+
+    // openMobNav.click(OpenningMobNav);
+    // closeMobNav.click(ClosingMobNav);
+    // function OpenningMobNav() {
+    //     mobileNav.css("visibility", "visible"); 
+    //     mainContent.css("visibility", "hidden");
+    // }
+    // function ClosingMobNav() {
+    //     mobileNav.css("visibility", "hidden"); 
+    //     mainContent.css("visibility", "visible");
+    // }
 
 });
